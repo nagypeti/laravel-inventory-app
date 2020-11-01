b3f7e45befb65e82101d0222b12f6b9758773edb
Peter Nagy <peter.nagy@adevinta.com>
2020-11-01 14:18:12 +0100

    Reorder layouts, create product form

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inventory App</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{ URL::to('products') }}">InventoryApp</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="{{ URL::to('products') }}">List products</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="container">
+        @yield('content')
+    </div>
+    <div class="text-center footer">
+        <h5>Contact</h5>
+        <h5>+36-70/414-9202</h5>
+        <h5>nagypeti222@gmail.com</h5>
+    </div>
+</body>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,42 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Inventory App</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
+@extends('layouts.app')
 
-    <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{ URL::to('products') }}">InventoryApp</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('products') }}">List products</a></li>
-        </ul>
-    </nav>
+@section('content')
 
-    <h1>Products</h1>
+<h1>Products</h1>
 
-    <table class="table table-striped table-bordered">
-        <thead>
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Price</td>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($products as $product)
         <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Price</td>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->price }}</td>
         </tr>
-        </thead>
-        <tbody>
-        @foreach($products as $product)
-            <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->price }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    @endforeach
+    </tbody>
+</table>
 
-</div>
-</body>
-</html>
+@endsection
