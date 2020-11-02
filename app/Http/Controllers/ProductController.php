5e3e504613382e9999d791eb4417043d6d1f294d
Peter Nagy <peter.nagy@adevinta.com>
2020-11-02 09:21:43 +0100

    Remove unused import

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProduct;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ProductController extends Controller
